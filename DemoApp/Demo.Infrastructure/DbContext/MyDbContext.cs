798fa1f1487b1e85d8221fb95ccaf8afdb933316
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-10-24 19:46:09 +0100

    Model Updated

--- DemoApp/Demo.Infrastructure/DbContext/MyDbContext.cs
+++ DemoApp/Demo.Infrastructure/DbContext/MyDbContext.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Demo.Domain.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Infrastructure
 {
     public class MyDbContext : DbContext
     {
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        public MyDbContext(DbContextOptions<MyDbContext> options) 
+            : base(options)
         {
         }
+
+        public DbSet<Fragrance> Fragrances { get; set; }
     }
 }
